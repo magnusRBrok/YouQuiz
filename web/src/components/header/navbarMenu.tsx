86d5521edf138504cff06bb0343d28583c2ad691
Anders Pedersen <afck97@hotmail.com>
2021-11-30 02:10:24 +0100

    updated navbar for auth/unauthed

--- web/src/components/header/navbarMenu.tsx
+++ web/src/components/header/navbarMenu.tsx
@@ -1,22 +1,36 @@
 import { Menu, MenuButton, MenuList, MenuItem } from "@chakra-ui/react";
-import { FC } from "react";
+import { FC, useContext } from "react";
 import { IconButton } from "@chakra-ui/button";
 import { HamburgerIcon } from "@chakra-ui/icons";
 import { Link } from "react-router-dom";
+import { StoreContext } from "../../contexts/AuthContext";
+import { observer } from "mobx-react-lite";
 
-const NavbarMenu: FC = ({ children }) => {
+const NavbarMenu: FC = () => {
+  const { authStore } = useContext(StoreContext);
+  const isAuthenticated = authStore.isAuthenticated();
+
   return (
     <Menu>
       <MenuButton icon={<HamburgerIcon />} as={IconButton} variant="outline" />
       <MenuList>
-        <Link to="/login">
-        <MenuItem>
-          <>Login</>
-        </MenuItem>
-        </Link>
+        {!isAuthenticated && (
+          <Link to="/login">
+            <MenuItem>
+              <>Login</>
+            </MenuItem>
+          </Link>
+        )}
+        {isAuthenticated && (
+          <Link to="/">
+            <MenuItem onClick={() => authStore.logout()}>
+              <>Logout</>
+            </MenuItem>
+          </Link>
+        )}
       </MenuList>
     </Menu>
   );
 };
 
-export default NavbarMenu;
+export default observer(NavbarMenu);
